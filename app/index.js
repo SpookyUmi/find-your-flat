74afdc1bb83a727aafc994c645f2f5119573fa8a
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-03-02 11:05:44 +0100

    change port

--- app/index.js
+++ app/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(router);
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server running, listening on port ${PORT}`);
 });
