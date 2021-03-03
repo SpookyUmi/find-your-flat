529b31c19e9af916024aff8e949407a0e1e6a086
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-03-03 19:30:27 +0100

    webhook first version

--- app/controller.js
+++ app/controller.js
@@ -9,25 +9,54 @@ const controller = {
   },
 
   slackHookSubmit(request, response, next) {
-    console.log('Body :', request.body);
+    console.log('yolo');
 
     if (request.body.adverts) {
       const flatToRent = request.body.adverts.map((flat) => (
         {
           "type": "section",
-          "text": "hello"
+          "text": {
+            "type": "mrkdwn",
+            "text": `*<${flat.url}|${flat.title}>*\n${flat.agency_name}\n${flat.price} e/mois\n${flat.area} mètres carrés\n${flat.bedrooms} chambres\nneuf ? ${flat.is_new}`
+          },
+          "accessory": {
+            "type": "image",
+            "image_url": flat.images_url[0],
+            "alt_text": "flat image"
+          }
+        },
+        {
+          "type": "context",
+          "elements": [
+            {
+              "type": "image",
+              "image_url": "https://api.slack.com/img/blocks/bkb_template_images/tripAgentLocationMarker.png",
+              "alt_text": "Location Pin Icon"
+            },
+            {
+              "type": "plain_text",
+              "emoji": true,
+              "text": `Location: ${flat.city}`
+            }
+          ]
+        },
+        {
+          "type": "divider"
         }
       ));
 
       axios.post(SLACK_URL,
         {
-          "blocks": flatToRent
+            "blocks": flatToRent
+        },
+        {
+          headers: { "Content-type": "application/json" }
         }
       )
 
-        response.send("It's an advert, cheers !");
+      return response.send("It's an advert, cheers !");
 
-    } response.send("Not an advert, but it's ok");
+    } return response.send("Not an advert, but it's ok");
   }
 }
 
