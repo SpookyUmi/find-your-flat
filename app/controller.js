40585956022d97e1f26cbaf45d23cf96cc55c396
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-03-03 19:55:59 +0100

    webhook is functional

--- app/controller.js
+++ app/controller.js
@@ -13,40 +13,25 @@ const controller = {
 
     if (request.body.adverts) {
       const flatToRent = request.body.adverts.map((flat) => (
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": `*<${flat.url}|${flat.title}>*\n${flat.agency_name}\n${flat.price} e/mois\n${flat.area} mètres carrés\n${flat.bedrooms} chambres\nneuf ? ${flat.is_new}`
-          },
-          "accessory": {
-            "type": "image",
-            "image_url": flat.images_url[0],
-            "alt_text": "flat image"
-          }
-        },
-        {
-          "type": "context",
-          "elements": [
-            {
-              "type": "image",
-              "image_url": "https://api.slack.com/img/blocks/bkb_template_images/tripAgentLocationMarker.png",
-              "alt_text": "Location Pin Icon"
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": `*<${flat.url}|${flat.title}>*\n${flat.agency_name}\n${flat.price} e/mois\n${flat.area} mètres carrés\n${flat.bedrooms} chambres\nneuf ? ${flat.is_new}`
             },
-            {
-              "type": "plain_text",
-              "emoji": true,
-              "text": `Location: ${flat.city}`
+            "accessory": {
+              "type": "image",
+              "image_url": flat.images_url[0],
+              "alt_text": "flat image"
             }
-          ]
-        },
-        {
-          "type": "divider"
-        }
+          }
       ));
+
+      console.log(flatToRent);
 
       axios.post(SLACK_URL,
         {
+            "text": "New flats",
             "blocks": flatToRent
         },
         {
