c3c5f08abd4cec9cf016b6d174219b14f7cdae1c
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-03-05 12:02:18 +0100

    add an other condition

--- app/controller.js
+++ app/controller.js
@@ -67,7 +67,61 @@ const controller = {
         });
         return response.send("It's an advert, cheers !");
 
-      } return response.send("Not an advert, but it's ok");
+    } else if (request.body) {
+      request.body.forEach(flat => {
+        let published = "";
+        if (flat.published_at) {
+          published = flat.published_at.replace(/T/g, ' ');
+        }
+        //Creating our "blocks" array
+        let flatToRent = [
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": `*<${flat.url}|${flat.title}>* ~ ${flat.agency_name}\n${flat.area} mètres carrés, ${flat.bedrooms} chambres, ${flat.price} e/mois\n*Neuf* : ${flat.is_new}\n*Publié à* : ${published}\n*Description* : ${flat.description}`
+            }
+          },
+        ];
+
+        if (flat.others) {
+          flat.others.assets.forEach(spec => {
+            const others = {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": `${spec}`
+              }
+            }
+            flatToRent.push(others);
+          })
+        }
+
+        flat.images_url.forEach(url => {
+          const images = {
+            "type": "image",
+            "image_url": url,
+            "alt_text": "flat image"
+          }
+          flatToRent.push(images);
+        })
+
+        axios.post(SLACK_URL,
+          {
+            "text": "New flats incoming !",
+            "blocks": flatToRent
+          },
+          {
+            headers: { "Content-type": "application/json" }
+          }
+        )
+      });
+      return response.send("It's an advert, cheers !");
+
+    } return response.send("Not an advert, but it's ok");
     }
   }
 
