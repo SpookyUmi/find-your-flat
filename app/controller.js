2fe2814bf7f77f2a3291d50abbe4672fe0392ded
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-03-09 23:52:59 +0100

    add alert for a buy search

--- app/controller.js
+++ app/controller.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const SLACK_URL = process.env.SLACK_URL;
+const SLACK_URL_MOM = process.env.SLACK_URL_MOM;
 
 const controller = {
   index(_, response) {
@@ -11,7 +12,7 @@ const controller = {
   slackHookSubmit(request, response, next) {
     console.log('yolo');
 
-    console.log("REQUÊTE :", request.body);
+    console.log("REQUÊTE :", request.body.adverts, "---------");
 
     if (request.body.adverts) {
       request.body.adverts.forEach(flat => {
@@ -20,7 +21,7 @@ const controller = {
           published = flat.published_at.replace(/T/g, ' ');
         }
         //Creating our "blocks" array
-        let flatToRent = [
+        let flatToCheck = [
           {
             "type": "divider"
           },
@@ -33,7 +34,7 @@ const controller = {
           },
         ];
 
-        if (flat.others) {
+        if (flat.others && flat.others.assets) {
           flat.others.assets.forEach(spec => {
             const others = {
               "type": "section",
@@ -42,7 +43,7 @@ const controller = {
                 "text": `${spec}`
               }
             }
-            flatToRent.push(others);
+            flatToCheck.push(others);
           })
         }
 
@@ -52,77 +53,134 @@ const controller = {
             "image_url": url,
             "alt_text": "flat image"
           }
-          flatToRent.push(images);
+          flatToCheck.push(images);
         })
-
-        axios.post(SLACK_URL,
-          {
-            "text": "New flats incoming !",
-            "blocks": flatToRent
-          },
-          {
-            headers: { "Content-type": "application/json" }
-          }
-          )
-        });
-        return response.send("It's an advert, cheers !");
-
-    } else if (request.body) {
-      request.body.forEach(flat => {
-        let published = "";
-        if (flat.published_at) {
-          published = flat.published_at.replace(/T/g, ' ');
+        axios.post(flat.ads_type === "buy" ? SLACK_URL_MOM : SLACK_URL,
+        {
+          "text": "Nouveaux logements à vendre !",
+          "blocks": flatToCheck
+        },
+        {
+          headers: { "Content-type": "application/json" }
         }
-        //Creating our "blocks" array
-        let flatToRent = [
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": `*<${flat.url}|${flat.title}>* ~ ${flat.agency_name}\n${flat.area} mètres carrés, ${flat.bedrooms} chambres, ${flat.price} e/mois\n*Neuf* : ${flat.is_new}\n*Publié à* : ${published}\n*Description* : ${flat.description}`
-            }
-          },
-        ];
-
-        if (flat.others) {
-          flat.others.assets.forEach(spec => {
-            const others = {
-              "type": "section",
-              "text": {
-                "type": "mrkdwn",
-                "text": `${spec}`
-              }
-            }
-            flatToRent.push(others);
-          })
-        }
-
-        flat.images_url.forEach(url => {
-          const images = {
-            "type": "image",
-            "image_url": url,
-            "alt_text": "flat image"
-          }
-          flatToRent.push(images);
-        })
-
-        axios.post(SLACK_URL,
-          {
-            "text": "New flats incoming !",
-            "blocks": flatToRent
-          },
-          {
-            headers: { "Content-type": "application/json" }
-          }
         )
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+        })
       });
+      // else if (request.body.adverts[0].ads_type === "buy") {
+      //   request.body.adverts.forEach(flat => {
+      //   let published = "";
+      //   if (flat.published_at) {
+      //     published = flat.published_at.replace(/T/g, ' ');
+      //   }
+      //   //Creating our "blocks" array
+      //   let flatToBuy = [
+      //     {
+      //       "type": "divider"
+      //     },
+      //     {
+      //       "type": "section",
+      //       "text": {
+      //         "type": "mrkdwn",
+      //         "text": `*<${flat.url}|${flat.title}>* ~ ${flat.agency_name}\n*Type* :${flat.property_type}\n${flat.area} mètres carrés, ${flat.bedrooms} chambres, ${flat.price} e\n*Neuf* : ${flat.is_new}\n*Publié à* : ${published}\n*Description* : ${flat.description}`
+      //       }
+      //     },
+      //   ];
+
+      //   if (flat.others && flat.others.assets) {
+      //     flat.others.assets.forEach(spec => {
+      //       const others = {
+      //         "type": "section",
+      //         "text": {
+      //           "type": "mrkdwn",
+      //           "text": `${spec}`
+      //         }
+      //       }
+      //       flatToBuy.push(others);
+      //     })
+      //   }
+
+      //   flat.images_url.forEach(url => {
+      //     const images = {
+      //       "type": "image",
+      //       "image_url": url,
+      //       "alt_text": "flat image"
+      //     }
+      //     flatToBuy.push(images);
+      //   })
+
+      //   axios.post(SLACK_URL_MOM,
+      //     {
+      //       "text": "Nouveaux logements à vendre !",
+      //       "blocks": flatToBuy
+      //     },
+      //     {
+      //       headers: { "Content-type": "application/json" }
+      //     }
+      //   )
+      // });
       return response.send("It's an advert, cheers !");
-
     } return response.send("Not an advert, but it's ok");
-    }
-  }
 
-  module.exports = controller;
+  }
+  // else if (request.body) {
+  //   request.body.forEach(flat => {
+  //     let published = "";
+  //     if (flat.published_at) {
+  //       published = flat.published_at.replace(/T/g, ' ');
+  //     }
+  //     //Creating our "blocks" array
+  //     let flatToRent = [
+  //       {
+  //         "type": "divider"
+  //       },
+  //       {
+  //         "type": "section",
+  //         "text": {
+  //           "type": "mrkdwn",
+  //           "text": `*<${flat.url}|${flat.title}>* ~ ${flat.agency_name}\n${flat.area} mètres carrés, ${flat.bedrooms} chambres, ${flat.price} e/mois\n*Neuf* : ${flat.is_new}\n*Publié à* : ${published}\n*Description* : ${flat.description}`
+  //         }
+  //       },
+  //     ];
+
+  //     if (flat.others) {
+  //       flat.others.assets.forEach(spec => {
+  //         const others = {
+  //           "type": "section",
+  //           "text": {
+  //             "type": "mrkdwn",
+  //             "text": `${spec}`
+  //           }
+  //         }
+  //         flatToRent.push(others);
+  //       })
+  //     }
+
+  //     flat.images_url.forEach(url => {
+  //       const images = {
+  //         "type": "image",
+  //         "image_url": url,
+  //         "alt_text": "flat image"
+  //       }
+  //       flatToRent.push(images);
+  //     })
+
+  //     axios.post(SLACK_URL,
+  //       {
+  //         "text": "New flats incoming !",
+  //         "blocks": flatToRent
+  //       },
+  //       {
+  //         headers: { "Content-type": "application/json" }
+  //       }
+  //     )
+  //   });
+  //   return response.send("It's an advert, cheers !");
+
+}
+
+module.exports = controller;
