3bf5a759fe3c0e2c585645573848d31fe341fabf
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-03-11 15:48:53 +0100

    change for an async await function

--- app/controller.js
+++ app/controller.js
@@ -9,181 +9,105 @@ const controller = {
     response.send('yolo');
   },
 
-  slackHookSubmit(request, response, next) {
-    console.log('yolo');
-
+  slackHookSubmit: async (request, response, next) => {
     console.log("REQUÊTE :", request.body.adverts, "---------");
-
-    if (request.body.adverts) {
-      request.body.adverts.forEach(flat => {
-        let published = "";
-        if (flat.published_at) {
-          published = flat.published_at.replace(/T/g, ' ');
-        }
-        let title = "";
-        if (flat.title.includes(`\t\t\t\t\t`)) {
-          title = flat.title.replace(/\t\t\t\t\t/g, ' ').replace(/\r\n/g, ' ');
-        }
-        //Creating our "blocks" array
-        let flatToCheck = [
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": `*<${flat.url}|${title.trim()}>* ~ ${flat.agency_name}\n${flat.area} mètres carrés, ${flat.bedrooms} chambres, ${flat.price} e/mois\n*Neuf* : ${flat.is_new}\n*Publié à* : ${published}\n*Description* : ${flat.description}`
-            }
-          },
-        ];
-
-        if (flat.others && flat.others.assets) {
-          flat.others.assets.forEach(spec => {
-            const others = {
+    try {
+      if (request.body.adverts) {
+        const receivedFlats = await request.body.adverts.forEach(flat => {
+          let published = "";
+          if (flat.published_at) {
+            published = flat.published_at.replace(/T/g, ' ');
+          }
+          let title = "";
+          if (flat.title.includes(`\t\t\t\t\t`)) {
+            title = flat.title.replace(/\t\t\t\t\t/g, ' ').replace(/\r\n/g, ' ');
+          }
+          //Creating our "blocks" array
+          let flatToCheck = [
+            {
+              "type": "divider"
+            },
+            {
               "type": "section",
               "text": {
                 "type": "mrkdwn",
-                "text": `${spec}`
+                "text": `*<${flat.url}|${title.trim()}>* ~ ${flat.agency_name}\n${flat.area} mètres carrés, ${flat.bedrooms} chambres, ${flat.price} e/mois\n*Neuf* : ${flat.is_new}\n*Publié à* : ${published}\n*Description* : ${flat.description}`
               }
-            }
-            flatToCheck.push(others);
-          })
-        }
+            },
+          ];
 
-        flat.images_url.forEach(url => {
-          const images = {
-            "type": "image",
-            "image_url": url,
-            "alt_text": "flat image"
+          if (flat.others && flat.others.assets) {
+            flat.others.assets.forEach(spec => {
+              const others = {
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": `${spec}`
+                }
+              }
+              flatToCheck.push(others);
+            })
           }
-          flatToCheck.push(images);
-        })
+
+          flat.images_url.forEach(url => {
+            const images = {
+              "type": "image",
+              "image_url": url,
+              "alt_text": "flat image"
+            }
+            flatToCheck.push(images);
+          })
+          axios.post(flat.ads_type === "buy" ? SLACK_URL_MOM : SLACK_URL,
+          {
+            "text": "New flats !",
+            "blocks": flatToCheck
+          },
+          {
+            headers: { "Content-type": "application/json" }
+          }
+          )
+          .then(response => {
+            console.log(response.status);
+          })
+          .catch(error => {
+            console.log(error);
+          })
+        });
+        console.log(receivedFlats);
+        return response.send("It's an advert, cheers !");
+      } return response.send("Not an advert, but it's ok");
+
+    } catch (error) {
+      console.log(error);
+      request.body.adverts.forEach((flat) => {
         axios.post(flat.ads_type === "buy" ? SLACK_URL_MOM : SLACK_URL,
-        {
-          "text": "New flats !",
-          "blocks": flatToCheck
-        },
-        {
-          headers: { "Content-type": "application/json" }
-        }
+          {
+            "text": "Une erreur est survenue",
+            "blocks": [
+              {
+                "type": "section",
+                "text": {
+                  "type": "plain_text",
+                  "text": `Une erreur est survenue : ${error}`,
+                  "emoji": true
+                }
+              }
+            ]
+          },
+          {
+            headers: { "Content-type": "application/json" }
+          }
         )
-        .then(response => {
-          console.log(response);
-        })
-        .catch(error => {
-          console.log(error);
-        })
-      });
-      // else if (request.body.adverts[0].ads_type === "buy") {
-      //   request.body.adverts.forEach(flat => {
-      //   let published = "";
-      //   if (flat.published_at) {
-      //     published = flat.published_at.replace(/T/g, ' ');
-      //   }
-      //   //Creating our "blocks" array
-      //   let flatToBuy = [
-      //     {
-      //       "type": "divider"
-      //     },
-      //     {
-      //       "type": "section",
-      //       "text": {
-      //         "type": "mrkdwn",
-      //         "text": `*<${flat.url}|${flat.title}>* ~ ${flat.agency_name}\n*Type* :${flat.property_type}\n${flat.area} mètres carrés, ${flat.bedrooms} chambres, ${flat.price} e\n*Neuf* : ${flat.is_new}\n*Publié à* : ${published}\n*Description* : ${flat.description}`
-      //       }
-      //     },
-      //   ];
-
-      //   if (flat.others && flat.others.assets) {
-      //     flat.others.assets.forEach(spec => {
-      //       const others = {
-      //         "type": "section",
-      //         "text": {
-      //           "type": "mrkdwn",
-      //           "text": `${spec}`
-      //         }
-      //       }
-      //       flatToBuy.push(others);
-      //     })
-      //   }
-
-      //   flat.images_url.forEach(url => {
-      //     const images = {
-      //       "type": "image",
-      //       "image_url": url,
-      //       "alt_text": "flat image"
-      //     }
-      //     flatToBuy.push(images);
-      //   })
-
-      //   axios.post(SLACK_URL_MOM,
-      //     {
-      //       "text": "Nouveaux logements à vendre !",
-      //       "blocks": flatToBuy
-      //     },
-      //     {
-      //       headers: { "Content-type": "application/json" }
-      //     }
-      //   )
-      // });
-      return response.send("It's an advert, cheers !");
-    } return response.send("Not an advert, but it's ok");
+          .then(response => {
+            console.log(response.status);
+          })
+          .catch(error => {
+            console.log(error);
+          })
+      })
+    }
 
   }
-  // else if (request.body) {
-  //   request.body.forEach(flat => {
-  //     let published = "";
-  //     if (flat.published_at) {
-  //       published = flat.published_at.replace(/T/g, ' ');
-  //     }
-  //     //Creating our "blocks" array
-  //     let flatToRent = [
-  //       {
-  //         "type": "divider"
-  //       },
-  //       {
-  //         "type": "section",
-  //         "text": {
-  //           "type": "mrkdwn",
-  //           "text": `*<${flat.url}|${flat.title}>* ~ ${flat.agency_name}\n${flat.area} mètres carrés, ${flat.bedrooms} chambres, ${flat.price} e/mois\n*Neuf* : ${flat.is_new}\n*Publié à* : ${published}\n*Description* : ${flat.description}`
-  //         }
-  //       },
-  //     ];
-
-  //     if (flat.others) {
-  //       flat.others.assets.forEach(spec => {
-  //         const others = {
-  //           "type": "section",
-  //           "text": {
-  //             "type": "mrkdwn",
-  //             "text": `${spec}`
-  //           }
-  //         }
-  //         flatToRent.push(others);
-  //       })
-  //     }
-
-  //     flat.images_url.forEach(url => {
-  //       const images = {
-  //         "type": "image",
-  //         "image_url": url,
-  //         "alt_text": "flat image"
-  //       }
-  //       flatToRent.push(images);
-  //     })
-
-  //     axios.post(SLACK_URL,
-  //       {
-  //         "text": "New flats incoming !",
-  //         "blocks": flatToRent
-  //       },
-  //       {
-  //         headers: { "Content-type": "application/json" }
-  //       }
-  //     )
-  //   });
-  //   return response.send("It's an advert, cheers !");
 
 }
 
